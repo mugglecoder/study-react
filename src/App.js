46fd57d7247efb361be8cc722994d7f5e17cc7ab
Park <winkknd@naver.com>
2019-04-22 00:04:56 +0900

    state

--- src/App.js
+++ src/App.js
@@ -2,24 +2,41 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movies = [
-  {
-    title: "fuck",
-    poster:
-      "https://regalcdn.azureedge.net/CaptainMarvel/HO00008345/TV_SmallPosterImage/20181219-113516399.jpg"
-  },
-  {
-    title: "test",
-    poster:
-      "https://cdn3.movieweb.com/i/movie/gEh4g0Db8qqSuAVOoGuEqYp9XRTdR2/384:50/Shazam.jpg"
-  }
-];
-
 class App extends Component {
+  state = {
+    movies: [
+      {
+        title: "fuck",
+        poster:
+          "https://regalcdn.azureedge.net/CaptainMarvel/HO00008345/TV_SmallPosterImage/20181219-113516399.jpg"
+      },
+      {
+        title: "test",
+        poster:
+          "https://cdn3.movieweb.com/i/movie/gEh4g0Db8qqSuAVOoGuEqYp9XRTdR2/384:50/Shazam.jpg"
+      }
+    ]
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: "War",
+            poster:
+              "https://regalcdn.azureedge.net/CaptainMarvel/HO00008345/TV_SmallPosterImage/20181219-113516399.jpg"
+          }
+        ]
+      });
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return (
             <Movie title={movie.title} poster={movie.poster} key={index} />
           );
