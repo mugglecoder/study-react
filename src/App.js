273239c6265094719f82d9886c59c71107c87a21
Park <winkknd@naver.com>
2019-04-22 03:54:45 +0900

    more props

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ class App extends Component {
     const movies = this.state.movies.map(movie => {
       return (
         <Movie
-          title={movie.title}
-          poster={movie.large_cover_image}
+          title={movie.title_english}
+          poster={movie.medium_cover_image}
           key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
         />
       );
     });
