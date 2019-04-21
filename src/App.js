099cf2433c6a1e7ea7ace49683c14c3ad4180e4c
Park <winkknd@naver.com>
2019-04-22 00:05:35 +0900

    state 이후 강의
    _renderMovies

--- src/App.js
+++ src/App.js
@@ -3,26 +3,22 @@ import "./App.css";
 import Movie from "./Movie";
 
 class App extends Component {
-  state = {
-    movies: [
-      {
-        title: "fuck",
-        poster:
-          "https://regalcdn.azureedge.net/CaptainMarvel/HO00008345/TV_SmallPosterImage/20181219-113516399.jpg"
-      },
-      {
-        title: "test",
-        poster:
-          "https://cdn3.movieweb.com/i/movie/gEh4g0Db8qqSuAVOoGuEqYp9XRTdR2/384:50/Shazam.jpg"
-      }
-    ]
-  };
+  state = {};
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "fuck",
+            poster:
+              "https://regalcdn.azureedge.net/CaptainMarvel/HO00008345/TV_SmallPosterImage/20181219-113516399.jpg"
+          },
+          {
+            title: "test",
+            poster:
+              "https://cdn3.movieweb.com/i/movie/gEh4g0Db8qqSuAVOoGuEqYp9XRTdR2/384:50/Shazam.jpg"
+          },
           {
             title: "War",
             poster:
@@ -30,17 +26,20 @@ class App extends Component {
           }
         ]
       });
-    }, 1000);
+    }, 4000);
   }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
 
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
